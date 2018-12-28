6dd71912b40d0b742b5a0ac2087ce89f4edf3b0a
lance <45931264+stu84085@users.noreply.github.com>
2018-12-29 01:06:21 +0800

    chg package

--- src/main/java/com/example/demo/MemberController.java
+++ src/main/java/com/example/demo/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.dao.UserRepository;
 import com.example.domain.User;
 
 @Controller
