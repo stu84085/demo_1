fe67f05bf536202e040e78724e8a16d7c3cd40de
lance <45931264+stu84085@users.noreply.github.com>
2019-01-15 16:42:13 +0800

    fix local ip & dockerFile

---
+++ src/main/java/com/example/demo/DockerController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @RequestMapping("/docker")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
